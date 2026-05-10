b28e1d0621987146c51fc223378c95cab0e32f31
Marian Dumitrescu <68550755+kyorri@users.noreply.github.com>
2026-05-10 16:52:47 +0300

    Added README docs and swagger-ui and redoc OpenAPI frontend (#2)

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 from .views import (
     UserViewSet,
     CategoryViewSet,
@@ -25,4 +28,7 @@ router.register(r"info", InfoPageViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", obtain_auth_token, name="api_token_auth"),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
